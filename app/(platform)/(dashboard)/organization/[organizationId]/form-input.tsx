f175576490297c5e4a5565508d90328ea3e5a0b5
minhvuong123 <minhvuong12222@gmail.com>
2024-02-14 20:53:59 +0700

    customize organization input form and useAction

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { useFormStatus } from "react-dom";
+
+interface FormInputProps {
+  errors?: {
+    title?: string[]
+  }
+}
+
+export const FormInput = ({ errors }: FormInputProps) => {
+  const { pending } = useFormStatus();
+  
+  return (
+    <div className="flex flex-col space-y-2">
+      <Input
+        type="text"
+        id="title"
+        name="title"
+        required
+        placeholder="Enter a board title"
+        disabled={pending}
+      />
+      {errors?.title?.map((error: string) => (
+        <p key={error} className="text-rose-500">
+          {error}
+        </p>
+      ))}
+    </div>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { State, create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
 import { useFormState } from "react-dom";
+import { FormInput } from "./form-input";
+import { FormButton } from "./form-button";
 
 export const Form = () => {
   const initialState = { message: null, errors: {} } as State;
@@ -11,21 +12,9 @@ export const Form = () => {
   return (
     <form action={dispatch}>
       <div className="flex flex-col space-y-2">
-        <input
-          type="text"
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a board title"
-          className="border-black border p-1"
-        />
-        {state?.errors?.title?.map((error: string) => (
-          <p key={error} className="text-rose-500">
-            {error}
-          </p>
-        ))}
+        <FormInput errors={state?.errors} />
       </div>
-      <Button type="submit">Submit</Button>
+      <FormButton />
     </form>
   );
 };
