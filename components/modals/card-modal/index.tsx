c28c3dcea607345cb95d3f1a5bc55a8643a27fb0
minhvuong123 <minhvuong12222@gmail.com>
2024-02-18 23:31:44 +0700

    handle add description for card

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -5,6 +5,9 @@ import { useCardModal } from "@/hooks/use-card-modal"
 import { fetcher } from "@/lib/fetcher";
 import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
+import { Header } from "./header";
+import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -19,7 +22,27 @@ export const CardModal = () => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
-        {cardData?.title}
+        {
+          !cardData
+          ?<Header.Skeleton />
+          :<Header data={cardData} />
+        }
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {
+                !cardData 
+                ? <Description.Skeleton />
+                : <Description data={cardData} />
+              }
+            </div>
+          </div>
+          {
+            !cardData 
+            ? <Actions.Skeleton />
+            : <Actions data={cardData} />
+          }
+        </div>
       </DialogContent>
     </Dialog>
   )
