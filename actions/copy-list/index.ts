45e63b8de7217e156bafd0d408f7c950ccd8d62d
minhvuong123 <minhvuong12222@gmail.com>
2024-02-19 14:17:23 +0700

    update log for all action in dialog card

--- actions/copy-list/index.ts
+++ actions/copy-list/index.ts
@@ -6,6 +6,8 @@ import { prismadb } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CopyList } from "./schema";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { createAuditLog } from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -64,6 +66,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       include: {
         cards: true
       }
+    })
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.CREATE
     })
   } catch (error) {
     console.log(error)
--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -27,6 +27,10 @@ export const Header = ({
         queryKey: ['card', data.id]
       })
 
+      queryClient.invalidateQueries({
+        queryKey: ['card-logs', data.id]
+      })
+
       toast.success(`Rename to "${data.title}"`);
       setTitle(data.title);
     },
